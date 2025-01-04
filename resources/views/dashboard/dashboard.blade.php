98b0129ce7e96d0261fe43e5c9784c9c0cd8fd71
Fahmirizal <fahmirizalbudiramadhan@gmail.com>
2025-01-04 17:34:06 +0700

    Menerapkan @include pada sidebar

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -5,41 +5,12 @@ Dashboard
 @section('body')
 <div class="container-fluid">
     <div class="row">
-        <!-- Sidebar -->
-        <div class="col-md-3 col-lg-2 bg-dark text-white p-3" style="min-height: 100vh;">
-            <h4 class="text-center">Dashboard</h4>
-            <ul class="nav flex-column">
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#home" data-bs-toggle="collapse">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#settings" data-bs-toggle="collapse">Settings</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#profile" data-bs-toggle="collapse">Profile</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#logout" data-bs-toggle="collapse">Logout</a>
-                </li>
-            </ul>
-        </div>
+        @include('include.sidebar')
         <!-- Main Content -->
         <div class="col-md-9 col-lg-10 p-4">
             <div id="home">
                 <h2>Home Page</h2>
                 <p>Selamat datang di dashboard utama!</p>
-            </div>
-            <div id="settings" class="collapse">
-                <h2>Settings</h2>
-                <p>Halaman pengaturan akan ditampilkan di sini.</p>
-            </div>
-            <div id="profile" class="collapse">
-                <h2>Profile</h2>
-                <p>Halaman profil pengguna.</p>
-            </div>
-            <div id="logout" class="collapse">
-                <h2>Logout</h2>
-                <p>Halaman logout.</p>
             </div>
         </div>
     </div>
---
+++ resources/views/include/sidebar.blade.php
@@ -0,0 +1,15 @@
+<!-- Sidebar -->
+<div class="col-md-3 col-lg-2 bg-dark text-white p-3" style="min-height: 100vh;">
+    <h4 class="text-center">Dashboard</h4>
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link text-white" href="/dashboard">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href="/kelola-konten">Kelola Konten</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href="">Kelola Kategori</a>
+        </li>
+    </ul>
+</div>
