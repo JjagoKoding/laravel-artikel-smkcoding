2da1e9cd9ce521133d9cc5e358d87c5ddf6b98d3
Fahmirizal <fahmirizalbudiramadhan@gmail.com>
2025-01-04 17:43:00 +0700

    Membuat Layout khusus dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,18 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 @section('title')
 Dashboard
 @endsection
-@section('body')
-<div class="container-fluid">
-    <div class="row">
-        @include('include.sidebar')
-        <!-- Main Content -->
-        <div class="col-md-9 col-lg-10 p-4">
-            <div id="home">
-                <h2>Home Page</h2>
-                <p>Selamat datang di dashboard utama!</p>
-            </div>
-        </div>
-    </div>
-</div>
+@section('isi')
+    <h2>Home Page</h2>
+    <p>Selamat datang di dashboard utama!</p>
 @endsection
